249e12a7e94bc9dfacfc47eca5285223633d6c91
paanSinghCoder <gaurav3017@gmail.com>
2024-09-16 22:28:31 +0530

    update script

--- script.sh
+++ script.sh
@@ -6,4 +6,4 @@ curl -X POST \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "Accept: application/vnd.github+json" \
   https://api.github.com/repos/paanSinghCoder/workflow-remote-dispatch/dispatches \
-  -d '{"event_type": "update-figma-variables", "client_payload": {"filename": "tokens.json", {"tokens":{"color":{"red":{"100":{"blendMode":"normal","description":"","extensions":{"org.lukasoppermann.figmaDesignTokens":{"exportKey":"color","styleId":"S:301e1c3f1b573356f7cab2ae123bad4fa671b31e"}},"type":"color","value":"#ef5da8ff"}}}}}}'
+  -d '{"event_type": "update-figma-variables", "client_payload": {"filename": "tokens.json", "tokens": {"color": {"red": {"100": {"blendMode": "normal", "description": "", "extensions": {"org.lukasoppermann.figmaDesignTokens": {"exportKey": "color", "styleId": "S:301e1c3f1b573356f7cab2ae123bad4fa671b31e"}}, "type": "color", "value": "#ef5da8ff"}}}}}}'

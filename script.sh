94b3ba56256737a685b19edae1f6f66888a85b6c
paanSinghCoder <gaurav3017@gmail.com>
2024-09-16 21:18:48 +0530

    remove tokens

--- script.sh
+++ script.sh
@@ -6,4 +6,4 @@ curl -X POST \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "Accept: application/vnd.github+json" \
   https://api.github.com/repos/paanSinghCoder/workflow-remote-dispatch/dispatches \
-  -d '{"event_type": "update-figma-variables", "client_payload": {"filename": "tokens.json", "tokens": {"color": "red"}}
+  -d '{"event_type": "update-figma-variables", "client_payload": {"filename": "tokens.json", "tokens": {"color": "red"}}'

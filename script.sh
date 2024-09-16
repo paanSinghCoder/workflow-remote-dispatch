ecceab0f07c9494c36e13f91dc793245696e9b98
paanSinghCoder <gaurav3017@gmail.com>
2024-09-16 11:16:33 +0530

    feat: remove comment

--- script.sh
+++ script.sh
@@ -2,21 +2,14 @@
 
 source .env
 
-# curl \
-#   -X POST \
-#   -H "Accept: application/vnd.github+json" \
-#   -H "Authorization: Bearer $GITHUB_TOKEN" \
-#   https://api.github.com/repos/paanSinghCoder/workflow-remote-dispatch/actions/workflows/117301642/dispatches \
-#   -d '{"ref":"main"}'
-
-  curl -X POST \
+curl -X POST \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
-  -H "Accept: application/vnd.github.v3+json" \
+  -H "Accept: application/vnd.github+json" \
   https://api.github.com/repos/paanSinghCoder/workflow-remote-dispatch/dispatches \
   -d '{
-    "event_type": "update-tokens",
+    "event_type": "update-figma-variables",
     "client_payload": {
         "filename": "design-tokens.tokens.json",
-        "commitMessage": ""
+        "commitMessage": "c"
     }
 }'

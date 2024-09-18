c2ca37df01fbb4d300676ef215cb7d06054b264a
paanSinghCoder <gaurav3017@gmail.com>
2024-09-18 12:26:21 +0530

    chore: add env example

--- script.sh
+++ script.sh
@@ -6,4 +6,4 @@ curl -X POST \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "Accept: application/vnd.github+json" \
   https://api.github.com/repos/paanSinghCoder/workflow-remote-dispatch/dispatches \
-  -d '{"event_type": "update-figma-variables", "client_payload": {"filename": "tokens.json", "tokens": {"color": {"red": {"primary": {"blendMode": "normal", "description": "", "extensions": {"org.lukasoppermann.figmaDesignTokens": {"exportKey": "color", "styleId": "S:301e1c3f1b573356f7cab2ae123bad4fa671b31e"}}, "type": "color", "value": "#868686"}}}}}}'
+  -d '{"event_type": "update-figma-variables", "client_payload": {"filename": "tokens.json", "tokens": {"color": {"orange": {"primary": {"blendMode": "normal", "description": "", "extensions": {"org.lukasoppermann.figmaDesignTokens": {"exportKey": "color", "styleId": "S:301e1c3f1b573356f7cab2ae123bad4fa671b31e"}}, "type": "color", "value": "#686868"}}}}}}'
